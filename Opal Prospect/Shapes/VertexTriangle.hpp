bd56cffc87921ed02be0e5818ee7db692186a4fb
swbengs <swbengs@yahoo.com>
2018-06-05 20:28:03 -0500

    Updated readme, code format, and design texts. Started adding methods to vertex triangle as well.

--- Opal Prospect/Shapes/VertexTriangle.hpp
+++ Opal Prospect/Shapes/VertexTriangle.hpp
@@ -41,7 +41,7 @@ public:
     void setA(Point3D point);
     void setB(Point3D point);
     void setC(Point3D point);
-    void set(Point3D a, Point3D b, Point3D c);
+    void setABC(Point3D a, Point3D b, Point3D c);
 
 private:
     Point3D point_a;
