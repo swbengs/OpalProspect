f97adc63abe15639557f027287f45df01bc65897
swbengs <swbengs@yahoo.com>
2018-08-25 19:35:23 -0500

    comment out older code. Played with the fps counter to not show as often

--- Opal Prospect/MainLoop.cpp
+++ Opal Prospect/MainLoop.cpp
@@ -104,10 +104,12 @@ void MainLoop::startLoop()
 
     std::cout << "\n";
 
-    const int target_fps = 60;
+    const unsigned int target_fps = 60;
+    const unsigned int fps_print_skip = 15;
     camera.setPosition(glm::vec3(0.0f, 185.0f, -10.0f));
 
-    int frames = target_fps;
+    unsigned int frames = target_fps;
+    unsigned int skips = 0;
     std::chrono::high_resolution_clock::time_point original_start = std::chrono::high_resolution_clock::now();
     std::chrono::high_resolution_clock::time_point start = original_start;
 
@@ -115,9 +117,14 @@ void MainLoop::startLoop()
     {
         if (frames == target_fps)
         {
+            skips++;
             std::chrono::duration<double> seconds = std::chrono::high_resolution_clock::now() - start;
-            std::cout << "seconds: " << seconds.count() << "\n";
-            std::cout << "fps: " << (static_cast<double>(frames) / seconds.count()) << "\n";
+            if (skips == fps_print_skip)
+            {
+                std::cout << "seconds: " << seconds.count() << "\n";
+                std::cout << "fps: " << (static_cast<double>(frames) / seconds.count()) << "\n";
+                skips = 0;
+            }
 
             frames = 0;
             start = std::chrono::high_resolution_clock::now();
